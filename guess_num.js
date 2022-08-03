24625b696eab2dde0c904c030aa5726bf9bf78ac
Shulaev <mail@ntdservice.ru>
2022-08-03 13:28:01 +0300

    guess_num add

---
+++ guess_num.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const rl = require('readline').createInterface(process.stdin, process.stdout)
+
+fs.mkdir('log', err=>{
+    if (err){  
+    } 
+    fs.open('log/testFile.txt', 'w', err=>{
+        if (err) throw err
+    });
+
+});
+
+function writeLog(trying, result, number){
+
+    fs.appendFile('log/testFile.txt', `Попытка: ${trying}, бросок игрока:${result}, генератор чисел: ${number} \n`, 
+                (err) => {
+                if(err) throw err;
+    });
+
+};
+
+function question(counter=0) {
+    
+    rl.question('Угадайте  число от 1 до 100: ', (cmd)=>{
+
+        if (cmd != 'quit' && isNaN(Number(cmd)) || cmd > 100 || cmd == '') {
+            console.log('Ошибка  ввода')
+            return question()
+        };
+
+        let number = Math.floor(Math.random() * 100)
+
+        if (cmd == 'quit') {
+            rl.close()
+            return
+        }else{
+            counter++
+            if (cmd == number) {
+                console.log(`вы угадали число ${number}!`)
+                writeLog(counter, cmd, number)
+            }
+            else if (cmd < number) {
+                console.log(`Ваше число меньше(${cmd}) чем ${number}!`)
+                writeLog(counter, cmd, number)
+            }
+            else {
+                console.log(`Ваше число больше(${cmd}) чем ${number}!`)
+                writeLog(counter, cmd, number)
+            };
+        };
+
+        next_quest = question(counter)
+    });
+
+};
+
+question()
+
+
+
