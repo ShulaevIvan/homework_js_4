d1462dbaa66a36ad2bb8f3f977fb1da639aa89ee
Shulaev <mail@ntdservice.ru>
2022-08-04 19:05:29 +0300

    rewrite async_await

--- guess_num_async.js
+++ guess_num_async.js
@@ -2,77 +2,61 @@ const fs = require('fs');
 const rl = require('readline').createInterface(process.stdin, process.stdout)
 
 
-nextQuest = function(counter=1) {
+fs.mkdirSync('log')
 
-    return  new Promise((resolve, reject)=>{
+async function writeLog(text) {
+  await fs.promises.appendFile('log/log.txt', text, {
+    encoding: 'utf8'
+  });
 
-        rl.question('Угадайте  число от 1 до 100: ', (cmd)=>{
+}
 
-            if (cmd != 'quit' && isNaN(Number(cmd)) || cmd > 100 || cmd == '' || cmd == 0){
-                reject('Ошибка  ввода')
-                return nextQuest()
-            };
+async function userCommand() {
+    let promise = new Promise(function(resolve, reject) {
+        rl.question('Введите число от 1 and 100: ', (cmd) => {
+            let number = cmd;
+            rl.pause();
+            return resolve(number); 
             
-            let number = Math.floor(Math.random(1) * 100)
-            if (cmd == number){
-                console.log(`вы угадали число ${number}!`)
-                writeLog(counter, cmd, number)
-                resolve(counter)
-            }
-            else if (cmd < number) {
-                console.log(`Ваше число меньше(${cmd}) чем ${number}!`)
-                writeLog(counter, cmd, number)
-                resolve(counter)
-            }
-            else {
-                console.log(`Ваше число больше(${cmd}) чем ${number}!`)
-                writeLog(counter, cmd, number)
-                resolve(counter)
-            };
-        });
+        });  
+    });
+    return await promise;
+}
 
-    }).then((counter)=>{
+async function play() {
+    let counter = 0;
+    while(true) {
+        let input = await userCommand(),
+            userNumber = Number(input),
+            randomNumber =  Math.floor(Math.random(1) * 100),
+            output = '';
+    
+        if(isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
+            output = 'Неправильный ввод\n';
+            writeLog(output)
+            console.log(output)
+            continue
+        };
         counter++
-        nextQuest(counter)
-    }).catch((err)=>{
-        console.log(err)
-    });
-};
 
-createDir = new Promise((resolve, reject)=>{
+        if(userNumber === randomNumber) {
+            output = `Вы угадали!: ${randomNumber}`
+            writeLog(output)
+            console.log(output)
+            break;
+        };
+    
+        if(userNumber > randomNumber) {
+            output =  `Ваше число больше(${userNumber}) чем ${randomNumber}! Попытка ${counter} \n`
+            writeLog(output)
+            console.log(output)
+        } else {
+            output =  `Ваше число меньше(${userNumber}) чем ${randomNumber}! Попытка ${counter} \n`
+            writeLog(output)
+            console.log(output)
+        }
+    }
+    rl.close()
+}
 
-    fs.mkdir('log', err=>{
-        if (err){ 
-            reject('папка существует \n') 
-        } 
-        fs.open('log/testFile.txt', 'w', err=>{
-            if (err) reject('ошибка создания/записи файла  \n')
-        });
-        resolve('создана директория \n')
-    });
-
-}).then((value)=>{
-    console.log(value)
-}).catch((err)=>{
-    console.log(err)
-}).finally(()=>{
-    nextQuest()
-});
-
-writeLog = function(trying, result, number){
-
-    return  new Promise((resolve, reject)=>{
-
-        fs.appendFile('log/testFile.txt', `Попытка: ${trying}, бросок игрока:${result}, генератор чисел: ${number} \n`, 
-            (err) => {
-            if(err) reject('ошибка записи в файл')
-            resolve('данные записаны')
-        });
-        
-    }).then((value)=>{
-        console.log(value)
-    }).catch((err)=>{
-        console.log(err)
-    });
-};
-
+play();
